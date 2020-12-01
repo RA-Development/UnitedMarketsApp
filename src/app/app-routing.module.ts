47dd716c10da5f9f2f4dcb575d650981327fa405
anneluong <54981148+anneluong@users.noreply.github.com>
2020-12-01 22:45:16 +0100

    Removed unused import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MarketsListComponent} from './components/markets/markets-list/markets-list.component';
 import {RouterModule, Routes} from '@angular/router';
 import {WelcomeComponent} from './components/welcome/welcome.component';
 import {MarketComponent} from './components/market/market.component';
